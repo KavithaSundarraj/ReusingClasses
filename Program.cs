7d4b8cd891588eaa2affed754c322bd9a2f7b7b2
kavitha sundarraj <jeyakumar.kavitha@gmail.com>
2020-11-03 18:08:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+/* 4.	Random List
+ * Date: 03 NOV 2020
+ * Create a RandomList class that has all the functionality of List<string>.
+Add additional function that returns and removes a random element from the list.
+•	Public method: RandomString(): string
+*/
+
+namespace ReusingClasses
+{
+    class RandomList
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random List");
+            List<String> names = new List<string>();
+            Console.WriteLine("Please enter the no. of names");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the names");
+            for (int i = 0; i < number; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            //To select random
+            RandomList RM = new RandomList();
+            names.Remove(RM.getRandomElement(names));
+            Console.WriteLine("After removal the available names are:");
+            foreach (String n in names)
+            Console.WriteLine(n);
+     }
+        public String getRandomElement(List<String> list)
+        {
+            Random rand = new Random();
+            int index = rand.Next(list.Count);
+            Console.WriteLine("Randomly selected name is:");
+            Console.WriteLine(list[index]);
+            return (list[index]);
+        }
+
+    }
+}
